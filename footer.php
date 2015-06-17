532ce6b3d368e8ab132a339586784e5ef8f0b9d1
monodnb <monodnb@gmail.com>
2015-06-17 13:33:49 +0300

    restructuring 2 / header & footer templates / html5 boilerplate head tag

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #main-view div and all content after
+ *
+ * @iscoutgames.com
+ */
+?>
+       
+    </main>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="scripts/vendor/jquery-2.1.4.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,52 +1,35 @@
 <?php
-session_start();
+    require_once 'header.php'
 ?>
+   
 
-<!doctype HTML>
-<html>
-
-<head>
-    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
-    <link type="text/css" rel="stylesheet" href="CSS/normalize.css" />
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="CSS/jquery-ui.css" />
-    <link type="text/css" rel="stylesheet" href="CSS/index.css" />
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-
-
-<body>
-    <div class="header">
-        <div class="logo"></div>
-        <div class="navigation">
-            <ul class="nav-menu">
-                <li> <a href="/"> Home </a></li>
-                <li> <a href="gameform.php"> Submit Game </a> </li>
-                <li> <a href="gamelist.php"> Game List </a> </li>            
-            </ul>        
-        </div>
-        <div id="account">
-            <ul>
-                <?php
-                    if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
-                { ?>
-                    <li><a href="account.php"> Account </a> </li>
-                    <li><a href="logout.php"> Log Out</a> </li>                    
-                <?php } else { ?>       
-                    <li><a href="login.php">Log In</a></li>       
-                    <li><a href="signup.php">Sign Up</a></li> 
-                <?php  } ?>
-            </ul>        
-        </div>  
+   <div class="header">
+    <div class="logo"></div>
+    <div class="navigation">
+        <ul class="nav-menu">
+            <li> <a href="/"> Home </a>
+            </li>
+            <li> <a href="gameform.php"> Submit Game </a> </li>
+            <li> <a href="gamelist.php"> Game List </a> </li>
+        </ul>
     </div>
-    <hr class="separator">
+    <div id="account">
+        <ul>
+            <?php if(!empty($_SESSION[ 'LoggedIn']) && !empty($_SESSION[ 'Username'])) { ?>
+            <li><a href="account.php"> Account </a> </li>
+            <li><a href="logout.php"> Log Out</a> </li>
+            <?php } else { ?>
+            <li><a href="login.php">Log In</a>
+            </li>
+            <li><a href="signup.php">Sign Up</a>
+            </li>
+            <?php } ?>
+        </ul>
+    </div>
+</div>
+<hr class="separator">
 
-    
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <script src="JS/jquery-ui.js"></script>
-    <script src="http://malsup.github.com/jquery.form.js"></script> 
-    <script src="JS/main.js"></script>
-</body>
-</html>
+
+<?php
+    require_once 'footer.php'
+?>
